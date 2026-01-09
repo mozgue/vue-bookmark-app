1466cd3b1b06625ea224872a74cf74ed35e7e42a
idorokhov <mozgue@gmail.com>
2026-01-09 09:11:36 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Vue Bookmark App
+
+## Start scaffold
+
+`npm create vue@latest`
+
+## Features
+
+- Vite + Vue 3 Composition API
+- TypeScript
+- Pinia (stat management) \*
+- Router (SPA development) \*
+- ESLint (error prevention)
+- Prettier (code formatting)
+
+\* - _будут добавлены вручную во время разработки_
+
+### Первый запуск
+
+<pre>
+npm i
+npm run format
+npm run dev
+</pre>
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app">
+    <aside class="aside">
+      <nav>Меню</nav>
+    </aside>
+    <main>Контент</main>
+  </div>
+  <header></header>
+</template>
+
+<style scoped>
+.app {
+  display: flex;
+  min-height: calc(100vh - 140px);
+  gap: 200px;
+  max-width: 1480px;
+  margin: 140px auto 0;
+}
+.aside {
+  min-width: 400px;
+}
+</style>
