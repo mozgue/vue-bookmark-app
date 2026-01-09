e3d1f7d078cb46271c5636cab9b46c1d81e51cdb
idorokhov <mozgue@gmail.com>
2026-01-09 12:44:25 +0300

    fetch переписан на axios

--- src/stores/profile.store.ts
+++ src/stores/profile.store.ts
@@ -1,14 +1,14 @@
-import { API_ROUTES } from '@/api';
+import { API_ROUTES, http } from '@/api';
 import type { Profile } from '@/interfaces/profile.interface';
+
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const useProfileStore = defineStore('profile', () => {
   const profile = ref<Profile>();
   const getProfile = async () => {
-    const response = await fetch(API_ROUTES.profile);
-    const res = (await response.json()) as Profile;
-    profile.value = res;
+    const { data } = await http.get<Profile>(API_ROUTES.profile);
+    profile.value = data;
   };
   return { profile, getProfile };
 });
