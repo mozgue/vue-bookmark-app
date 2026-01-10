a2e60f979a8e3329e6763730fb1946ef5c757a45
idorokhov <mozgue@gmail.com>
2026-01-10 15:07:11 +0300

    Добавил иконки

--- src/icons/IconLogout.vue
+++ src/icons/IconLogout.vue
@@ -1,30 +1,25 @@
-<script setup>
-  const {color = "white", size = 24} = defineProps({
-    color: String,
-    size: Number
-  })
+<script setup lang="ts">
+const { color = 'white', size = 16 } = defineProps<{ color?: string; size?: number }>();
 </script>
 
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    :stroke="color"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    :style="{ scale: size / 24 }"
+    :width="size"
+    :height="size"
+    :fill="color"
+    class="bi bi-box-arrow-right"
+    viewBox="0 0 16 16"
   >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
-    <path d="M9 12h12l-3 -3" />
-    <path d="M18 15l3 -3" />
+    <path
+      fill-rule="evenodd"
+      d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
+    />
+    <path
+      fill-rule="evenodd"
+      d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
+    />
   </svg>
 </template>
-
-<script setup lang="ts"></script>
 
 <style scoped></style>
